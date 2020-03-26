9b05dc141dee2852d9359925497f930bc5eff054
Akos Szabo-Gali <szabogaliakos@gmail.com>
2020-03-26 04:11:38 +0100

    Form, Table for Student objects implemented

--- student.js
+++ student.js
@@ -1,19 +1,29 @@
-function Person(_firstName, _lastName, _gender, _date){
-    this.firstName = _firstName;
-    this.lastName = _lastName;
-    this.gender = _gender;
-    this.date = _date;
+class Person{
+    constructor(_firstName, _lastName, _gender, _date) {
+        this.firstName = _firstName;
+        this.lastName = _lastName;
+        this.gender = _gender;
+        this.date = _date;
+    }
+    fullName() {
+        return firstName + " " + lastName;
+    }
+    personInfo() {
+        return firstName + " " + lastName + ", " + gender + ", " + date;
+    }
 }
-Person.prototype.fullName = function(){
-    return firstName + " " + lastName;
-};
-Person.prototype.personInfo = function(){
-    return firstName + " " + lastName + ", " + gender + ", " + date;
-};
 
-function Student(_class, _faculty, _major){
-    this.class = _class;
-    this.faculty = _faculty;
-    this.major = _major;
+class Student extends Person{
+    constructor(_firstName, _lastName, _gender, _date, _class, _faculty, _major) {
+        super(_firstName, _lastName, _gender, _date);
+        this.class = _class;
+        this.faculty = _faculty;
+        this.major = _major;
+    }
 }
+
+
+
+
+
 
